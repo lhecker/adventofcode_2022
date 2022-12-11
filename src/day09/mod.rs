8cd31137f75d14a65bed33c8517d561609ced7e0
Leonard Hecker <leonard@hecker.io>
2022-12-11 20:16:01 +0100

    Improve day 9

--- src/day09/mod.rs
+++ src/day09/mod.rs
@@ -6,19 +6,6 @@ pub fn day09() -> Result<(usize, usize)> {
     const WIDTH: i32 = 768;
     const HEIGHT: i32 = 512;
 
-    const N: i32 = 0;
-    const L: i32 = -1;
-    const R: i32 = 1;
-    const U: i32 = -1;
-    const D: i32 = 1;
-    const MOVES: [[(i32, i32); 5]; 5] = [
-        [(L, U), (L, U), (N, U), (R, U), (R, U)],
-        [(L, U), (N, N), (N, N), (N, N), (R, U)],
-        [(L, N), (N, N), (N, N), (N, N), (R, N)],
-        [(L, D), (N, N), (N, N), (N, N), (R, D)],
-        [(L, D), (L, D), (N, D), (R, D), (R, D)],
-    ];
-
     let mut total1 = 1usize;
     let mut total2 = 1usize;
     let mut knots = [(WIDTH / 2, HEIGHT / 2); 10];
@@ -43,10 +30,10 @@ pub fn day09() -> Result<(usize, usize)> {
         }
 
         let d = match ascii[0] {
-            b'L' => (L, 0),
-            b'R' => (R, 0),
-            b'U' => (0, U),
-            b'D' => (0, D),
+            b'L' => (-1, 0),
+            b'R' => (1, 0),
+            b'U' => (0, -1),
+            b'D' => (0, 1),
             _ => bail!("dir"),
         };
         let steps: u32 = line[2..].parse()?;
@@ -62,14 +49,20 @@ pub fn day09() -> Result<(usize, usize)> {
             for i in 1..knots.len() {
                 let h = knots[i - 1];
                 let t = knots[i];
-                // Using unchecked access here makes it run roughly 15% faster.
-                let d = unsafe {
-                    MOVES
-                        .get_unchecked((h.1 - t.1 + 2) as usize)
-                        .get_unchecked((h.0 - t.0 + 2) as usize)
-                };
-                knots[i].0 += d.0;
-                knots[i].1 += d.1;
+                let dx = h.0 - t.0;
+                let dy = h.1 - t.1;
+                // Normally a `dx.clamp(-1, 1)` (or `dy...`) would be sufficient,
+                // but I found that a LUT is significantly faster.
+                // Using unchecked access here makes it run roughly 5-10% faster. Rust doesn't
+                // know nor understand the the game fundamentally doesn't allow a dx/dy of >2.
+                const LUT: [i32; 5] = [-1, -1, 0, 1, 1];
+                let dx_clamped = unsafe { *LUT.get_unchecked((dx + 2) as usize) };
+                let dy_clamped = unsafe { *LUT.get_unchecked((dy + 2) as usize) };
+                // Equivalent to `if dx.abs() > 1 || dy.abs() > 1` but slightly faster.
+                if dx != dx_clamped || dy != dy_clamped {
+                    knots[i].0 += dx_clamped;
+                    knots[i].1 += dy_clamped;
+                }
             }
 
             total1 += visit(knots[1], 1);
